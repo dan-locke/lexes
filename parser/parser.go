e281b7bb4ec44ba654f775536fc6e6f541f28011
Daniel Locke <locke.dj@hotmail.com>
2017-08-18 23:26:08 +1000

    Fixed nest span

--- parser/parser.go
+++ parser/parser.go
@@ -5,7 +5,10 @@ import (
 	"reflect"
 	"strings"
 	"encoding/json"
+	"fmt"
 )
+
+const FALSE_SPAN_AND int = 1000000
 
 const WITHIN_PARA string = "50"
 const WITHIN_SENT string = "20"
@@ -201,86 +204,165 @@ func assignTtype(token string) ttype {
 	return Phrase
 }
 
-func nodeToJson(n Node, field string) interface{} {
-	clause := map[string][]interface{}{}
+func nodeToJson(n Node, field string, span_child bool) interface{} {
+	// clause := map[string][]interface{}{}
+	var clause interface{}
+
+	if n.Slop || span_child {
+		clause = handleSpanOperator(n, field)
+	} else {
+		// clause = map[string][]interface{}{}
+		children := make([]interface{}, len(n.Children))
 	
-	if n.Slop {
-		children := len(n.Children)
-		clauses := make([]interface{}, children)
-		for i := 0; i < children; i++ {
+		for i := range n.Children {
 			if reflect.TypeOf(n.Children[i]) == reflect.TypeOf("") {
-				clauses[i] = map[string]interface{}{
-					"span_term" : map[string]interface{}{
-						field : n.Children[i],
-					},
-				}
+				// Change here to change for where seen_span
+
+				children[i] = parseTerm(n.Children[i].(string), n.Type[i], field, span_child)
 			} else {
 				// Parse node recursively 		
-				clauses[i] = nodeToJson(n.Children[i].(Node), field)
+				children[i] = nodeToJson(n.Children[i].(Node), field, span_child)
 			}
 		}
-		node := map[string]interface{}{
+		// clause[n.Operator] = append(clause[n.Operator], nodeToJson(n.Children[i].(Node), field, span_child))
+		clause = map[string]interface{} {
+			n.Operator : children,
+		}
+	}
+	
+	return clause
+}
+
+func handleSpanOperator(n Node, field string) *map[string]interface{} {
+	clauses := make([]interface{}, len(n.Children))
+	
+	for i := range n.Children {
+		if reflect.TypeOf(n.Children[i]) == reflect.TypeOf("") {
+			clauses[i] = parseTerm(n.Children[i].(string), n.Type[i], field, true)
+		} else {
+			// Parse node recursively 		
+			clauses[i] = nodeToJson(n.Children[i].(Node), field, true)
+		}
+	}
+	
+	node := map[string]interface{}{}
+	if n.Slop {
+		node = map[string]interface{}{
 			"span_near" : map[string]interface{}{
 				"clauses" : clauses, 
 				"slop" : n.Proximity,
 				"in_order" : false,
 			},
 		}
-		clause[n.Operator] = append(clause[n.Operator], node)
-	} else {
-		for i := range n.Children {
-			if reflect.TypeOf(n.Children[i]) == reflect.TypeOf("") {
-				clause[n.Operator] = append(clause[n.Operator], parseTerm(n.Children[i].(string), n.Type[i], field))
-			} else {
-				// Parse node recursively 		
-				clause[n.Operator] = append(clause[n.Operator], nodeToJson(n.Children[i].(Node), field))
-			}
+	} else if n.Operator == "must" {
+		node = map[string]interface{}{
+			"span_near" : map[string]interface{}{
+				"clauses" : clauses, 
+				"slop" : FALSE_SPAN_AND,
+				"in_order" : false,
+			},
 		}
-	}
-
-	return clause
+	} else if n.Operator == "should" { 
+		node = map[string]interface{}{
+			"span_or" : map[string]interface{}{
+				"clauses" : clauses,
+				"in_order" : false,
+			},
+		}
+	} 
+	
+	// clause[n.Operator] = append(clause[n.Operator], node)
+	fmt.Println(node)
+	return &node
 }
 
-func parseTerm(term string, t ttype, field string) *map[string]interface{} {
+func parseTerm(term string, t ttype, field string, span bool) *map[string]interface{} {
 
 	clause := map[string]interface{}{}
 
-	switch(t) {
-		case Phrase:
-			clause = map[string]interface{}{
-				"term" : map[string]interface{}{
-					field : term,
-				},
-			}
-			break
+	if span { 
+		switch(t) {
+			case Phrase:
+				clause = map[string]interface{}{
+					"span_term" : map[string]interface{}{
+						field : term,
+					},
+				}
+				break
 
-		case Prefix:
-			clause = map[string]interface{}{
-				"prefix" : map[string]interface{}{
-					field : term,
-				},
-			}
-			break
+			case Prefix:
+				clause = map[string]interface{}{
+					"span_multi" : map[string]interface{}{
+						"match" : map[string]interface{}{
+							"prefix" : map[string]interface{}{
+								field : term,
+							},
+						},
+					},
+				}
+				break
 
-		case Wildcard:
-			clause = map[string]interface{}{
-				"wildcard" : map[string]interface{}{
-					field : term,
-				},
-			}
-			break
+			case Wildcard:
+				clause = map[string]interface{}{
+					"span_multi" : map[string]interface{}{
+						"match" : map[string]interface{}{
+							"wildcard" : map[string]interface{}{
+								field : term,
+							},
+						},
+					},
+				}
+				break
+		}
+	} else {
+		switch(t) {
+			case Phrase:
+				clause = map[string]interface{}{
+					"term" : map[string]interface{}{
+						field : term,
+					},
+				}
+				break
+
+			case Prefix:
+				clause = map[string]interface{}{
+					"prefix" : map[string]interface{}{
+						field : term,
+					},
+				}
+				break
+
+			case Wildcard:
+				clause = map[string]interface{}{
+					"wildcard" : map[string]interface{}{
+						field : term,
+					},
+				}
+				break
+		}
 	}
 
 	return &clause
 }
 
 func parseToJson(n *Node, field string, retrieve []string, highlight bool) ([]byte, error) {
-	query := nodeToJson(*n, field)
-	res := map[string]interface{}{
-		"query" : map[string]interface{}{
-			"bool" : query,
-		},
+	
+	query := nodeToJson(*n, field, false)
+
+	res := map[string]interface{}{}
+	
+	if n.Slop { 
+		res = map[string]interface{}{
+			"query" : query,
+		}
+	} else {
+		res = map[string]interface{}{
+			"query" : map[string]interface{}{
+				"bool" : query,
+			},
+		}
 	}
+
 
 	if len(retrieve) != 0 {
 		res["_source"] = retrieve
